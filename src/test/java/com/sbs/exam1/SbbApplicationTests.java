7abceed5c4f4631ebe3c9d20cf7a75629fd1bed7
rantinum <syjcode5724@gmail.com>
2023-08-30 09:27:31 +0900

    Answer

--- src/test/java/com/sbs/exam1/SbbApplicationTests.java
+++ src/test/java/com/sbs/exam1/SbbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Exam1ApplicationTests {
+class SbbApplicationTests {
 
 	@Test
 	void contextLoads() {
