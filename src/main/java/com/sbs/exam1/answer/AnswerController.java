3a8d9e309a45e5c77d4f858e42b541c3d20d5b1d
rantinum <syjcode5724@gmail.com>
2023-08-31 15:43:50 +0900

    Question Registration & Question Form

--- src/main/java/com/sbs/exam1/answer/AnswerController.java
+++ src/main/java/com/sbs/exam1/answer/AnswerController.java
@@ -2,13 +2,14 @@ package com.sbs.exam1.answer;
 
 import com.sbs.exam1.question.Question;
 import com.sbs.exam1.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -16,15 +17,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
     private final QuestionService questionService;
-
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
-        Question question =  questionService.getQuestion(id);
-
-        this.answerService.create(question, content);
-        // TODO: 답변을 저장한다.
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm, BindingResult bindingResult) {
+        Question question = this.questionService.getQuestion(id);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
